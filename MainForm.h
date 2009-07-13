391c300e9d7f28c4d5fcb12b3724058a1181bf50
Tapdingo <tapdingo@googlemail.com>
2009-07-13 22:16:40 +0200

    [basic] Implemented the first skeleton of the app

--- MainForm.h
+++ MainForm.h
@@ -1,5 +1,11 @@
 #ifndef MAINFORM_H
 #define MAINFORM_H
 
+#include <QWidget>
+
+class MainForm : public QWidget
+{
+	Q_OBJECT
+};
 
 #endif
--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@
 int main(int argc, char* argv[])
 {
 	QApplication app(argc, argv);
+	MainForm mainForm;
+	mainForm.show();
 	return app.exec();
 }
