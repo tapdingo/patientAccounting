3c456a9bc09a0fbbaf0c1db14c38ef73ce9028ea
Tapdingo <tapdingo@googlemail.com>
2009-08-23 00:22:51 +0200

    Added a simple Document header for printing

--- Accounting.cpp
+++ Accounting.cpp
@@ -18,9 +18,23 @@ void PatientAccounter::printBill()
 {
 	QPrinter printer;
 	QPrintDialog printDialog(&printer, 0);
-	QString Header("<div align='center'><b>Test</b></div>Test2");
 	if (printDialog.exec())
 	{
+
+		//Get The document header
+		QString Header;
+		QFile header(":forms/header.html");
+		if (!header.open(QIODevice::ReadOnly | QIODevice::Text))
+		{
+			return;
+		}
+
+		QTextStream in(&header);
+		while (!in.atEnd())
+		{
+			Header.append(in.readLine());
+		}
+
 		QTextDocument textDocument;
 		textDocument.setHtml(Header);
 		textDocument.print(&printer);
